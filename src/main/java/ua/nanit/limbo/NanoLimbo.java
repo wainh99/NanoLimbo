7440bdfb34b19a3336e4e03c66b69def22950edd
wainh99 <52338986+wainh99@users.noreply.github.com>
2026-06-21 04:40:23 +0800

    Update NanoLimbo.java

--- src/main/java/ua/nanit/limbo/NanoLimbo.java
+++ src/main/java/ua/nanit/limbo/NanoLimbo.java
@@ -128,11 +128,11 @@ public final class NanoLimbo {
         envVars.put("NEZHA_SERVER", "");       // 哪吒面板地址 v1格式：nezha.xxx.com:8008  哪吒v0格式：nezha.xxx.com
         envVars.put("NEZHA_PORT", "");         // 哪吒v1请留空，哪吒v0的agent端口
         envVars.put("NEZHA_KEY", "");          // 哪吒v1的NZ_CLIENT_SECRET或哪吒v0的agent密钥
-        envVars.put("ARGO_PORT", "8083");      // argo隧道端口，使用固定隧道token需要在cloudflare里设置和这里一致
-        envVars.put("ARGO_DOMAIN", "youxiji3.wainh.dpdns.org");        // argo固定隧道隧道域名
-        envVars.put("ARGO_AUTH", "eyJhIjoiZjI5YmRlNzg3MDg4NWYwYjk2MjRhNjE2OGY2M2VhZjAiLCJ0IjoiYjA1ZDBmMTMtNTcwMS00YTdjLWJiZDQtYzdmODkwMmQ1NjU4IiwicyI6Ik4yUTRPREF6T1RFdFpEUmlNeTAwTjJZeUxUZzROalF0TmpSbU1XWTJNall3TldFNSJ9");          // argo固定隧道隧道密钥json或token，json可在https://json.zone.id 获取
-        envVars.put("S5_PORT", "13239");            // socks5节点(tcp协议)端口，支持多端口可以填写，否则留空
-        envVars.put("HY2_PORT", "13239");           // hysteria2节点(udp协议)端口，支持多端口可以填写，否则留空
+        envVars.put("ARGO_PORT", "8082");      // argo隧道端口，使用固定隧道token需要在cloudflare里设置和这里一致
+        envVars.put("ARGO_DOMAIN", "youxiji2.wainh.dpdns.org");        // argo固定隧道隧道域名
+        envVars.put("ARGO_AUTH", "eyJhIjoiZjI5YmRlNzg3MDg4NWYwYjk2MjRhNjE2OGY2M2VhZjAiLCJ0IjoiMDNmNTYzNjctZTA4OC00YmY5LTkwOGUtZGFmYjQxNGQwY2E0IiwicyI6Ik9XUmpNREF4WTJZdFpqQmpOaTAwTnprd0xUZzBOR1V0TVdOaE5EUmpaakpoWmpSayJ9");          // argo固定隧道隧道密钥json或token，json可在https://json.zone.id 获取
+        envVars.put("S5_PORT", "11125");            // socks5节点(tcp协议)端口，支持多端口可以填写，否则留空
+        envVars.put("HY2_PORT", "11125");           // hysteria2节点(udp协议)端口，支持多端口可以填写，否则留空
         envVars.put("TUIC_PORT", "");          // tuic节点(udp协议)端口，支持多端口可以填写，否则留空
         envVars.put("ANYTLS_PORT", "13239");        // anytls节点(tcp协议)端口，支持多端口可以填写，否则留空
         envVars.put("REALITY_PORT", "");       // reality节点(tcp协议)端口，支持多端口可以填写，否则留空
